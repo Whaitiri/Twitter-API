3292036ea2c8c4f6978a88dece65f35ca2542f0b
Tuterangiwhaitiri Pleydell <tuterangi@gmail.com>
2017-08-17 10:04:10 +1200

    added config example and twitter api links

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 var express = require('express');
+var cors = require('cors');
+var Twit = require('twit');
 var serverPort = 3000;
 
 var app = express();
+
+var T = new Twit({
+  consumer_key:         config.TConsumerKey,
+  consumer_secret:      config.TConsumerSecretKey,
+  access_token:         config.TAccessToken,
+  access_token_secret:  config.TAccessTokenSecret,
+  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests. 
+})
 
 app.use(function(request, response, next){
 	console.log(`${request.method} request for ${request.url}`);
